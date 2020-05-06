4f6f1fd8cee10811619cde0d2f962c271dbc6dd8
Anjan Srihari <anjansrihari14@gmail.com>
2020-05-06 17:34:58 +0530

    First Push

---
+++ src/JSON.hs
@@ -0,0 +1,92 @@
+module JSON (
+        module JSON
+            ) where
+
+import TinyParse
+import qualified Data.Map.Strict as Map
+import System.IO(readFile)
+import Data.Maybe(fromMaybe)
+import Data.Text.Prettyprint.Doc
+
+-- Pretty Printing Instance for Map
+instance (Pretty k, Pretty v, Ord k) => Pretty (Map.Map k v) where
+        pretty a = vsep ([pretty '{'] ++ (list f t) ++ [pretty '}']) 
+                where
+                   t        = Map.toList a
+                   f (k,v)  = 
+                        indent 2 $ ((fillBreak 10 $ pretty k) 
+                             <+> hsep [pretty ':', pretty v , pretty ','])
+                   f' (k,v)  = 
+                        indent 2 $ ((fillBreak 10 $ pretty k) 
+                             <+> hsep [pretty ':', pretty v])
+                   list f t = let n = init t
+                                  l = last t in (map f n) ++ [f' l]
+
+-- Pretty Printing instance for JsonVal
+instance Pretty JsonVal where
+        pretty (JsonNull)      = pretty "null"
+        pretty (JsonBool a)    = pretty $ if a then "true" else "false"
+        pretty (JsonStr a)     = pretty '\"' <> pretty a <> pretty '\"'
+        pretty (JsonNum a)     = pretty a
+        pretty (JsonObj m)     = pretty m
+        pretty (JsonArr a)     = prettyList a 
+
+data JsonVal = JsonNull 
+             | JsonBool Bool 
+             | JsonStr String
+             | JsonNum Double
+             | JsonObj (Map.Map (String) JsonVal) 
+             | JsonArr [JsonVal]
+             deriving (Show,Eq)
+
+jsonNull :: Parser JsonVal
+jsonNull = JsonNull <$ string "null"
+
+jsonBool :: Parser JsonVal 
+jsonBool = true <|> false
+           where
+                   true  = JsonBool True  <$ string "true"
+                   false = JsonBool False <$ string "false"
+
+jsonStr :: Parser JsonVal
+jsonStr =  JsonStr <$> (char '"' *> stringLiteral <* char '"')
+
+jsonNum :: Parser JsonVal
+jsonNum = JsonNum <$> number
+
+jsonArr :: Parser JsonVal
+jsonArr = JsonArr <$> (char '[' *> sepBy com element <* char ']')
+          where
+                  com = char ','
+
+jsonObj :: Parser JsonVal
+jsonObj =  f <$> (char '{' *> sepBy com member <* char '}')
+          where
+                  com = char ','
+                  f :: [(JsonVal,JsonVal)] -> JsonVal
+                  f x = JsonObj <$> Map.fromList $ 
+                          map (\(JsonStr a, b) -> (a,b)) x 
+
+element :: Parser JsonVal
+element = (whiteSpace *> jsonVal <* whiteSpace)
+
+member :: Parser (JsonVal,JsonVal)
+member = comb <$> (whiteSpace *> jsonStr <* whiteSpace) <*> char ':' <*> element
+         where
+                comb :: JsonVal -> Char -> JsonVal -> (JsonVal,JsonVal)
+                comb key _ val = (key,val) 
+
+jsonVal :: Parser JsonVal
+jsonVal = jsonNull <|> jsonBool <|> jsonStr <|> jsonArr <|> jsonObj <|> jsonNum
+
+parsePrintFile   :: String -> (IO (Doc ann))
+parsePrintFile inp = do
+                   tok  <- loadJsonFromFile inp 
+                   return $ pretty tok
+
+loadJsonFromFile :: String -> (IO JsonVal)
+loadJsonFromFile inp = do
+                  str <- readFile inp
+                  let 
+                    (tok,_) = fromMaybe (JsonNull, "") (parse jsonVal str)
+                  return tok
---
+++ src/TinyParse.hs
@@ -0,0 +1,103 @@
+module TinyParse (
+        Parser(parse),
+        number,
+        stringLiteral,
+        string,
+        sepBy,
+        whiteSpace,
+        item,
+        char,
+        character,
+        escape,
+        satisfy, 
+        module Control.Applicative,
+        module Control.Monad,
+        module Data.Char        
+        ) where
+
+import Control.Applicative
+import Control.Monad
+import Data.Char
+
+newtype Parser a = Parser { parse :: String -> Maybe (a,String) }
+
+instance Functor Parser where
+        fmap f (Parser p) = 
+                Parser $ \input -> do 
+                                (a,input') <- p input
+                                return (f a, input')
+
+instance Applicative Parser where
+        pure x = Parser $ \input -> Just (x,input)
+        (Parser p1) <*> (Parser p2) =
+                Parser $ \input -> do
+                        (f,input')  <- p1 input
+                        (x,input'') <- p2 input'
+                        return (f x, input'')
+
+instance Alternative Parser where
+        empty = Parser $ \_ -> empty
+        (Parser p1) <|> (Parser p2) =
+                Parser $ \input -> (p1 input) <|> (p2 input)
+
+instance Monad Parser where
+        return           = pure
+        (Parser p) >>= f =
+                Parser $ \input -> do
+                        (a,rest) <- p input
+                        parse (f a) rest
+
+item :: Parser Char
+item = Parser $ 
+        \input -> case input of
+             []   -> Nothing
+             x:xs -> return (x,xs)
+
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy pred = do
+        c <- item
+        if pred c then return c else empty
+
+char :: Char -> Parser Char
+char = satisfy . (==)
+
+string :: String -> Parser String
+string = sequenceA . map char
+
+sepBy :: Parser a -> Parser b -> Parser [b]
+sepBy sep p  = (sep `sepBy1` p) <|> pure []
+
+sepBy1 :: Parser a -> Parser b -> Parser [b]
+sepBy1 sep p = (:) <$> p <*> many (sep *> p)
+
+character :: Parser Char
+character = satisfy (liftA2 (&&) (/= '"') (/= '\\'))
+
+escape :: Parser Char
+escape  =
+        ( '"' <$ string "\\\"") <|>     
+        ('\\' <$ string "\\\\") <|>     
+        ('/' <$ string "\\/" ) <|>      
+        ('\b' <$ string "\\b" ) <|>     
+        ('\f' <$ string "\\f" ) <|>     
+        ('\n' <$ string "\\n" ) <|>     
+        ('\r' <$ string "\\r" ) <|>     
+        ('\t' <$ string "\\t" )
+
+whiteSpace :: Parser String
+whiteSpace = many (satisfy isSpace)
+
+-- order matters here
+number :: Parser Double
+number = read <$> (negParse <|> decParse <|> intParse)
+         where
+                digit = satisfy isDigit 
+                intParse = many digit
+                decParse = combine <$>  intParse <*> char '.' <*> intParse
+                negParse = (:) <$> char '-' <*> (decParse <|> intParse)
+                
+                combine :: String -> Char -> String -> String
+                combine base point decimal = base ++ (point : decimal)
+
+stringLiteral :: Parser String
+stringLiteral = many (character <|> escape)
